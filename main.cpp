52455103c92f2a2d1d20e64021d5e4482a30cd67
Dimitri Ter-Saakov <terdimitri@gmail.com>
2019-09-16 09:10:37 -0400

    compiled main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
+struct Word {
+    unsigned char bytes[5];
+    bool sign;
+}
+
+struct ShortWord {
+    unsigned char bytes[2];
+    bool sign;
+}
+
+
 int main() {
-	if (1 << 4 > 15) {
-		std::cout << "he is big";
-	} else {
-		std::cout << "he is not";
-	}
+    if (1 << 4 > 15) {
+        std::cout << "he is big\n";
+    } else {
+        std::cout << "he is not\n";
+    }
+
+    return 0;
 }
