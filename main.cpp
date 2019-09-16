035de17d850ea718f5fdd9ef3a53b09314e047a1
Dimitri Ter-Saakov <terdimitri@gmail.com>
2019-09-16 11:55:32 -0400

    Added iterpret method to turn Word or ShortWord into intergers.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
+#define BYTE_SIZE 6
 
 struct Word {
     unsigned char bytes[5];
     bool sign;
-}
+};
 
 struct ShortWord {
     unsigned char bytes[2];
     bool sign;
+};
+
+Word memory[4000];
+
+long int interpret(Word word) {
+    long int out = 0;
+    int i = 0;
+    for (int byte : word.bytes) {
+        out += (long int) byte << (BYTE_SIZE*i);
+        std::cout << out << "\n";
+        i++;
+    }
+    if (word.sign) {out *= -1;}
+    return out;
 }
 
-
 int main() {
-    if (1 << 4 > 15) {
-        std::cout << "he is big\n";
-    } else {
-        std::cout << "he is not\n";
-    }
-
+    Word word = {{12, 1, 0, 0, 0}, true};
+    std::cout << interpret(word) << "\n";
     return 0;
 }
