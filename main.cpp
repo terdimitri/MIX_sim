22e4c9e68212deb19e79c88ed415323b3c28593b
Dimitri Ter-Saakov <terdimitri@gmail.com>
2019-09-19 10:09:40 -0400

    Added registers, comp ind, overflow ind, instruction pointer, and main loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define BYTE_SIZE 6
+#define MEM_SIZE 4000
 
 struct Word {
     unsigned char bytes[5];
@@ -11,14 +12,24 @@ struct ShortWord {
     bool sign;
 };
 
-Word memory[4000];
+enum CompState {GREATER, EQUAL, LESS};
 
 long int interpret(Word word) {
     long int out = 0;
     int i = 0;
     for (int byte : word.bytes) {
         out += (long int) byte << (BYTE_SIZE*i);
-        std::cout << out << "\n";
+        i++;
+    }
+    if (word.sign) {out *= -1;}
+    return out;
+}
+
+long int interpret(ShortWord word) {
+    long int out = 0;
+    int i = 0;
+    for (int byte : word.bytes) {
+        out += (long int) byte << (BYTE_SIZE*i);
         i++;
     }
     if (word.sign) {out *= -1;}
@@ -26,7 +37,30 @@ long int interpret(Word word) {
 }
 
 int main() {
-    Word word = {{12, 1, 0, 0, 0}, true};
-    std::cout << interpret(word) << "\n";
+    Word memory[MEM_SIZE];
+    CompState comp_ind;
+    bool overflow;
+    Word rA;
+    Word rX;
+    ShortWord rI[6];
+    ShortWord rJ;
+
+    int pointer = 0;
+    unsigned char C, I, F;
+    int M;
+    while (pointer > MEM_SIZE) {
+        C = memory[pointer].bytes[4];
+        F = memory[pointer].bytes[3];
+        I = memory[pointer].bytes[2];
+        M = memory[pointer].bytes[0] << BYTE_SIZE + memory[pointer].bytes[1];
+        if (memory[pointer].sign) {M *= -1;}
+
+        if (I != 0) {
+            M += interpret(rI[I-1]);
+        }
+    }
+    memory[0] = {{12, 1, 0, 0, 0}, true};
+    std::cout << "memory 0: " << interpret(memory[0]) << "\n";
+    std::cout << "rJ sign state: " << rJ.sign << "\n";
     return 0;
 }
